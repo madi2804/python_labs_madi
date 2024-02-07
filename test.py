73cfebce89ad7afde3cc07d9696019e391b17080
Madi <m.kumarov@bytepride.dev>
2024-02-08 02:01:21 +0600

    from home to casting

--- test.py
+++ test.py
@@ -1,1 +1,97 @@
-print('hello world')
+# Exercise 1
+print("Hello World")
+
+# Exercise 2
+if 5 > 2:
+    print("YES")
+
+# Exercise 3
+# This is a comment
+
+# Exercise 4
+"""
+This is a comment
+written in 
+more than just one line
+"""
+
+# Exercise 5
+carname = "Volvo"
+
+# Exercise 6
+x = 50 
+
+# Exercise 7
+x = 5
+y = 10
+print(x+y)
+
+# Exercise 8
+x = 5
+y = 10
+z = x+y
+print(z)
+
+# Exercise 9
+x, y, z = "Orange", "Banana", "Cherry"
+
+# Exercise 10
+x = y = z = "Orange"
+
+# Exercise 11
+def myfunc():
+    global x
+    x = "fantastic"
+
+# Exercise 12
+x = 5
+print(type(x))
+int
+
+# Exercise 13
+x = "Hello World"
+print(type(x))
+str
+
+# Exercise 14
+x = 20.5
+print(type(x))
+float
+
+# Exercise 15
+x = ["apple", "banana", "cherry"]
+print(type(x))
+list
+
+# Exercise 16
+x = ("apple", "banana", "cherry")
+print(type(x))
+tuple
+
+# Exercise 17
+x = {"name" : "John", "age" : 36}
+print(type(x))
+dict
+
+# Exercise 18
+x = True
+print(type(x))
+bool
+
+# Exercise 19
+x = 5
+x = float(x)
+
+# Exercise 20
+x = 5.5
+x = int(x)
+
+# Exercise 21
+x = 5
+x = complex(x)
+
+
+#Exercise 22
+import random
+
+print(random.randrange(1, 10))
